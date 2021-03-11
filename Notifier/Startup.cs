d92a0a762ff79b5c519cf45fa90c12b05482a5c5
Thomas Starr-Timberlake <tls62m@mail.umkc.edu>
2021-03-11 15:38:00 -0600

    imported code for actual login pages, modified password requirements

--- Notifier/Startup.cs
+++ Notifier/Startup.cs
@@ -34,6 +34,16 @@ namespace Notifier
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+			services.Configure<IdentityOptions>(options =>
+			{
+				// Default Password settings.
+				options.Password.RequireDigit = true;
+				options.Password.RequireLowercase = true;
+				options.Password.RequireNonAlphanumeric = true;
+				options.Password.RequireUppercase = true;
+				options.Password.RequiredLength = 8;
+				options.Password.RequiredUniqueChars = 1;
+			});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
