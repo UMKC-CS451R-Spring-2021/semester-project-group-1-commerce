97319e9d7046e86435dbab5ec6eb53b4e58e7203
Thomas Starr-Timberlake <tstarr.timberlake@gmail.com>
2021-04-13 19:22:14 -0500

    filter test, rule model

---
+++ Notifier/Models/NotificationRule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Notifier.Models
+{
+    public class NotificationRule
+    {
+		public int RuleID { get; set; }
+
+		// user ID from AspNetUser table.
+		public string OwnerID { get; set; }
+
+		[Display(Name = "Date Filter")]
+		public String BeforeAfterDate;
+
+		[Display(Name = "Date")]
+		[DataType(DataType.Date)]
+		public DateTime TransactionDateFilter { get; set; }
+
+		[Display(Name = "Timer Filter")]
+		public String BeforeAfterTime;
+
+		[Display(Name = "Time")]
+		[DataType(DataType.Time)]
+		public DateTime TransactionTimeFilter { get; set; }
+
+		[Display(Name = "Location")]
+		public string LocationFilter { get; set; }
+
+		[Display(Name = "Deposit/Withdrawl")]
+		public string DepositWithdrawlFilter { get; set; }
+
+		[Display(Name = "Transaction Limiter")]
+		public String MoreLessEqualTrans;
+
+		[Display(Name = "Transaction Amount")]
+		[Column(TypeName = "decimal(18, 2)")]
+		public decimal TransAmountFilter { get; set; }
+
+		public string DescriptionFilter { get; set; }
+	}
+}
---
+++ Notifier/Pages/Transactions/FilteredTransactions.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notifier.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Notifier.Data;
+
+namespace Notifier.Pages.Transactions
+{
+    public class FilteredTransactionsModel : PagesTransactions.DI_BasePageModel
+    {
+        //private readonly ApplicationDbContext _context;
+
+        public FilteredTransactionsModel(
+        ApplicationDbContext context,
+        IAuthorizationService authorizationService,
+        UserManager<IdentityUser> userManager)
+        : base(context, authorizationService, userManager)
+        {
+        }
+
+        public IList<Transaction> Transaction { get;set; }
+
+        public async Task OnGetAsync()
+        {
+                var Transactions = from c in Context.Transaction
+                                   select c;
+
+                var currentUserId = UserManager.GetUserId(User);
+
+                Transactions = Transactions.Where(c => c.Location.Contains("SPAIN")
+                                       && c.OwnerID == currentUserId);
+
+                Transaction = await Transactions.ToListAsync();
+        }
+    }
+}
